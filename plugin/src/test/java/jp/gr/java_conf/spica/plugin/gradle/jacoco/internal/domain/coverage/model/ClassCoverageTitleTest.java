59c4039c23a2d85c59f561a868411677467c1b79
Ramiro Aparicio <rapariciog@gmail.com>
2021-09-28 17:24:54 +0200

    Fix tests

--- plugin/src/test/java/jp/gr/java_conf/spica/plugin/gradle/jacoco/internal/domain/coverage/model/ClassCoverageTitleTest.java
+++ plugin/src/test/java/jp/gr/java_conf/spica/plugin/gradle/jacoco/internal/domain/coverage/model/ClassCoverageTitleTest.java
@@ -9,12 +9,12 @@ class ClassCoverageTitleTest {
   @Test
   void toString_returns_worstN() {
     assertThat(new ClassCoverageTitle(new ClassCoverageLimit(3)))
-        .hasToString("Class list with less coverage (Worst 3)");
+        .hasToString("Class list with less coverage (Worst 3)\n");
   }
 
   @Test
   void toString_returns_without_limit() {
     assertThat(new ClassCoverageTitle(new ClassCoverageLimit(0)))
-        .hasToString("Class list with less coverage");
+        .hasToString("Class list with less coverage\n");
   }
 }
