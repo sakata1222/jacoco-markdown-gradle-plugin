f99b5b0d34c34f2dcbcfbdc076321a0154b52b10
sakata1222 <sakata1222@gmail.com>
2023-03-19 10:26:05 +0900

    refactor: do not use deprecated api

--- plugin/src/main/java/jp/gr/java_conf/spica/plugin/gradle/jacoco/internal/utils/GradleVersionSupport.java
+++ plugin/src/main/java/jp/gr/java_conf/spica/plugin/gradle/jacoco/internal/utils/GradleVersionSupport.java
@@ -1,5 +1,6 @@
 package jp.gr.java_conf.spica.plugin.gradle.jacoco.internal.utils;
 
+import groovy.lang.Closure;
 import org.gradle.api.file.FileSystemLocation;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.reporting.ConfigurableReport;
@@ -21,6 +22,25 @@ public class GradleVersionSupport {
           .invoke(configurableReport);
     } catch (ReflectiveOperationException e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  public static <T> T configureSelf(Closure configureClosure, T target) {
+    // org.gradle.util.ConfigureUtil is deprecated,
+    // but org.gradle.util.internal.ConfigureUtil which is available in gradle 7.1 or later
+    // is not deprecated
+    try {
+      return org.gradle.util.internal.ConfigureUtil.configureSelf(configureClosure, target);
+    } catch (NoClassDefFoundError | NoSuchMethodError e) {
+      // for gradle 6.x and 7.0
+      try {
+        return (T) Class.forName("org.gradle.util.ConfigureUtil")
+            .getMethod("configureSelf", Closure.class, Object.class)
+            .invoke(null, configureClosure, target);
+      } catch (ReflectiveOperationException ex) {
+        throw new RuntimeException(ex);
+      }
     }
   }
 }
